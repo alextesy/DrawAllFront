37d599a77ff3a02cf167c24000ae4f4f2843c223
Alexander Kremyanskyy <ALEXKREM@ibm.com>
2018-06-13 21:45:50 +0300

    server change

--- js.js
+++ js.js
@@ -2,7 +2,7 @@
     var canvas = document.getElementById("imgCanvas");
     var context = canvas.getContext("2d");
     canvas.addEventListener('click',createElement,false )
-    serverAddress='http://localhost:8080/';
+    serverAddress='https://drawall.azurewebsites.net/';
     var shape='Triangle';
     var color='black';
     var size=50;
